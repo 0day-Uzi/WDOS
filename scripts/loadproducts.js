f9df75dc2bad83d1bec9a6c359b3449e1e7c9c9e
Uzman Badurdeen <187439757+0day-Uzi@users.noreply.github.com>
2025-04-20 21:42:25 +0530

    Updated Correct json Path and CSS Linking

--- scripts/loadproducts.js
+++ scripts/loadproducts.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("../scripts/products.json")
+    fetch(" ../scripts/products.json")
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("products-container");
+
+            data.categories.forEach(category => {
+                const section = document.createElement("section");
+                section.classList.add("category-section");
+
+                const title = document.createElement("h2");
+                title.textContent = category.category;
+                section.appendChild(title);
+
+                const grid = document.createElement("div");
+                grid.classList.add("product-grid");
+
+                category.products.forEach(product => {
+                    const card = document.createElement("div");
+                    card.classList.add("product-card");
+
+                    card.innerHTML = `
+                    <img src="${product.image}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                    <p>${product.specs}</p>
+                    ${!product.isPlaceholder ? `<p class="price">LKR ${product.price.toLocaleString()}</p>
+                    <button>${product.buttonText}</button>` : `<p class="coming-soon">More options arriving soon.</p>`}
+                `;
+
+                grid.appendChild(card);
+            });
+
+            section.appendChild(grid);
+            container.appendChild(section);
+        });
+
+        })
+        .catch(error => {
+            console.error("failed to load products;", error);
+        })
 });
+    
+
+
+
+
