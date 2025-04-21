df5525c3456a68bb4b3577f25189ca85ebb2d51c
Uzman Badurdeen <187439757+0day-Uzi@users.noreply.github.com>
2025-04-22 00:43:50 +0530

    Added error handling

--- scripts/loadproducts.js
+++ scripts/loadproducts.js
@@ -1,23 +1,45 @@
+let shoppingCart = [];
+const MAX_STOCK = 10;
+
+function formatPrice(price) {
+    return 'LKR ' + parseFloat(price).toLocaleString('en-US' , {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })
+}
 document.addEventListener("DOMContentLoaded", () => {
     fetch("scripts/products.json")
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to load the products");
+            }
+            return response.json();
+        })
         .then(data => {
             const container = document.getElementById("products-container");
 
+            //section for each category
             data.categories.forEach(category => {
+                if (!category.products || category.products.length ===0)
+                return;
+
                 const section = document.createElement("section");
-                section.classList.add("category-section");
+                section.className = "category-section";
 
                 const title = document.createElement("h2");
                 title.textContent = category.category;
                 section.appendChild(title);
 
+
                 const grid = document.createElement("div");
-                grid.classList.add("product-grid");
+                grid.className = "product-grid";
 
                 category.products.forEach(product => {
+                    //No price products (the more ram coming soon part)
+                    if (product.price === "0 LKR") return;
+
                     const card = document.createElement("div");
-                    card.classList.add("product-card");
+                    card.className= "product-card";
 
                     card.innerHTML = `
                     <img src="${product.image}" alt="${product.name}">
